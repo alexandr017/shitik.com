0d9f7c0935c6c01eeb8b891396c03a150eb3ea9f
alexandr017 <sasha17@live.ru>
2025-04-11 15:42:32 +0300

    bug fixed

--- routes/includes/services.php
+++ routes/includes/services.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'ru/services/world-count' => 'view/v3/templates/services/world-count.php',
-    'ru/services/my-books-grades' => 'view/v3/templates/services/my-books-grade.php',
+    'ru/services/world-count' => 'resources/views/v3/templates/services/world-count.php',
+    'ru/services/my-books-grades' => 'resources/views/v3/templates/services/my-books-grade.php',
 ];
